d2001a24f97451036a706180e642ea6e7951f259
Almukanov <ruslan.almukanov@magenta-technology.com>
2025-01-03 00:30:13 +0500

    020125 - skeleton 1.1.1

--- src/main/java/com/ieltsdemo/model/question/Question.java
+++ src/main/java/com/ieltsdemo/model/question/Question.java
@@ -1,15 +1,26 @@
 package com.ieltsdemo.model.question;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Data;
 import org.hibernate.annotations.UuidGenerator;
 import org.springframework.data.annotation.Id;
 
-
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "type" // Поле, по которому определяется тип вопроса
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = OpenQuestion.class, name = "OPEN"),
+        @JsonSubTypes.Type(value = ClosedQuestion.class, name = "CLOSED")
+})
 @Data
 public abstract class Question {
     @Id
     @UuidGenerator
     private String id;
+    private TechnicalDetails technicalDetails;
     private int num;
     private String text;
     private String correctAnswer;
