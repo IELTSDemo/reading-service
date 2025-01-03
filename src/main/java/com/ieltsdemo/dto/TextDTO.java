c4696f7a98a41cc2c9e8a07ce0997ab578c6934f
Almukanov <ruslan.almukanov@magenta-technology.com>
2025-01-04 02:55:51 +0500

    040125 - MVP 1.1.5 corrected 2

--- src/main/java/com/ieltsdemo/dto/TextDTO.java
+++ src/main/java/com/ieltsdemo/dto/TextDTO.java
@@ -10,4 +10,28 @@ public class TextDTO {
         this.title = title;
         this.textNumber = textNumber;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTextNumber() {
+        return textNumber;
+    }
+
+    public void setTextNumber(String textNumber) {
+        this.textNumber = textNumber;
+    }
 }
