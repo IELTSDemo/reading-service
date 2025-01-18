204d5ac12d686ba4e3e0d8aeb01dc2584b583ea5
Almukanov <ruslan.almukanov@magenta-technology.com>
2025-01-18 15:45:57 +0500

    test

--- src/main/java/com/ieltsdemo/controller/TextController.java
+++ src/main/java/com/ieltsdemo/controller/TextController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/texts")
+@RequestMapping("/apis/texts")
 public class TextController {
 
     private final TextService textService;
